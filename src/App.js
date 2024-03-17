6ef2561a049cf612d5e16e7303a458c6b88095e0
Tanishe Boucher <115950194+tanisheboucher@users.noreply.github.com>
2024-03-17 10:02:31 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import WeatherApp from './Components/WeatherApp/WeatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+      <h6>
+      <a href='bouchers.com'>Open-source code</a> by Tanishe Boucher
+      </h6>
+    </div>
+    
+    
+  );
+}
+
+export default App;
---
+++ src/Components/WeatherApp/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="body">
+        <img src="../Assets" alt="" className="bg" />
+    </div>
+  )
+}
+
+export default Footer
